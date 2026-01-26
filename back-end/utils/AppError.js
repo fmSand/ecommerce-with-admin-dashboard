af51c509e7394bce786eca09c03f29d9aacc67ec
fmSand <fridamsand@gmail.com>
2026-01-26 14:16:22 +0100

    feat: create AppError class for standardized error handling

---
+++ back-end/utils/AppError.js
@@ -0,0 +1,9 @@
+class AppError extends Error {
+  constructor(statusCode, message, extraData = {}) {
+    super(message);
+    this.statusCode = statusCode;
+    this.extraData = extraData;
+  }
+}
+
+module.exports = AppError;
