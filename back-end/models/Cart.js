0760890c171a0eb030964af52bdcbc001faf34fb
fmSand <fridamsand@gmail.com>
2026-01-26 14:04:07 +0100

    feat: add Cart and CartItem models with associations

---
+++ back-end/models/Cart.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Cart = sequelize.define(
+    "Cart",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      tableName: "carts",
+      timestamps: true,
+    }
+  );
+
+  Cart.associate = (models) => {
+    Cart.belongsTo(models.User, { foreignKey: "userId", as: "user", onDelete: "CASCADE" });
+    Cart.hasMany(models.CartItem, { foreignKey: "cartId", as: "items", onDelete: "CASCADE" });
+  };
+
+  return Cart;
+};
---
+++ back-end/models/CartItem.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const CartItem = sequelize.define(
+    "CartItem",
+    {
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        validate: { min: 1 },
+      },
+      cartId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "cartItems",
+      timestamps: false,
+      indexes: [
+        {
+          unique: true,
+          fields: ["cartId", "productId"],
+        },
+      ],
+    }
+  );
+
+  CartItem.associate = (models) => {
+    CartItem.belongsTo(models.Cart, { foreignKey: "cartId", as: "cart", onDelete: "CASCADE" });
+    CartItem.belongsTo(models.Product, { foreignKey: "productId", as: "product", onDelete: "RESTRICT" });
+  };
+
+  return CartItem;
+};
