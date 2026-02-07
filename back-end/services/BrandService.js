6b00dfe699a9b193151112126fa902e004c8153a
fmSand <fridamsand@gmail.com>
2026-02-07 18:08:18 +0100

    feat: implement simple CRUD BrandService and CategoryService

---
+++ back-end/services/BrandService.js
@@ -0,0 +1,43 @@
+const { AppError } = require("../utils");
+
+class BrandService {
+  constructor(db) {
+    this.Brand = db.Brand;
+    this.Product = db.Product;
+  }
+
+  async getById(id) {
+    const brand = await this.Brand.findByPk(id);
+    if (!brand) throw new AppError(404, "Brand not found");
+    return brand;
+  }
+
+  async getAll() {
+    return this.Brand.findAll({ order: [["name", "ASC"]] });
+  }
+
+  async create(name) {
+    return this.Brand.create({ name });
+  }
+
+  async update(id, name) {
+    const brand = await this.Brand.findByPk(id);
+    if (!brand) throw new AppError(404, "Brand not found");
+    await brand.update({ name });
+    return brand;
+  }
+
+  async delete(id) {
+    const brand = await this.Brand.findByPk(id);
+    if (!brand) throw new AppError(404, "Brand not found");
+
+    const productCount = await this.Product.count({ where: { brandId: id } });
+    if (productCount > 0) {
+      throw new AppError(400, "Cannot delete brand with existing products");
+    }
+
+    await brand.destroy();
+  }
+}
+
+module.exports = BrandService;
---
+++ back-end/services/CategoryService.js
@@ -0,0 +1,43 @@
+const { AppError } = require("../utils");
+
+class CategoryService {
+  constructor(db) {
+    this.Category = db.Category;
+    this.Product = db.Product;
+  }
+
+  async getById(id) {
+    const category = await this.Category.findByPk(id);
+    if (!category) throw new AppError(404, "Category not found");
+    return category;
+  }
+
+  async getAll() {
+    return this.Category.findAll({ order: [["name", "ASC"]] });
+  }
+
+  async create(name) {
+    return this.Category.create({ name });
+  }
+
+  async update(id, name) {
+    const category = await this.Category.findByPk(id);
+    if (!category) throw new AppError(404, "Category not found");
+    await category.update({ name });
+    return category;
+  }
+
+  async delete(id) {
+    const category = await this.Category.findByPk(id);
+    if (!category) throw new AppError(404, "Category not found");
+
+    const productCount = await this.Product.count({ where: { categoryId: id } });
+    if (productCount > 0) {
+      throw new AppError(400, "Cannot delete category with existing products");
+    }
+
+    await category.destroy();
+  }
+}
+
+module.exports = CategoryService;
