50cadc3cd61f5977a4dfa0be71caa8735cb5c1f3
fmSand <fridamsand@gmail.com>
2026-02-09 12:15:01 +0100

    refactor: add validation to user role update and user update routes

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
-const { asyncHandler, authenticate, requireAdmin, requireSelfOrAdmin } = require("../middleware");
+const { asyncHandler, validate, authenticate, requireAdmin, requireSelfOrAdmin } = require("../middleware");
+const { updateUserSchema, updateRoleSchema, idParamSchema } = require("../validation");
 const { getAllUsers, getUserById, updateUser, updateUserRole } = require("../controllers/userController");
 
 router.get("/", authenticate, requireAdmin, asyncHandler(getAllUsers));
-router.put("/:id/role", authenticate, requireAdmin, asyncHandler(updateUserRole));
-router.get("/:id", authenticate, requireSelfOrAdmin, asyncHandler(getUserById));
-router.put("/:id", authenticate, requireSelfOrAdmin, asyncHandler(updateUser));
-//validate param etc
+router.put(
+  "/:id/role",
+  validate(idParamSchema, "params"),
+  authenticate,
+  requireAdmin,
+  validate(updateRoleSchema),
+  asyncHandler(updateUserRole),
+);
+router.get("/:id", authenticate, requireSelfOrAdmin, validate(idParamSchema, "params"), asyncHandler(getUserById));
+router.put(
+  "/:id",
+  validate(idParamSchema, "params"),
+  authenticate,
+  requireSelfOrAdmin,
+  validate(updateUserSchema),
+  asyncHandler(updateUser),
+);
+
 module.exports = router;
