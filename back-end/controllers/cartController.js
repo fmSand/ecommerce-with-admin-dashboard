445b657312713751d19b6332a26f6016aca6cc5f
fmSand <fridamsand@gmail.com>
2026-02-10 12:56:34 +0100

    feat: implement cartController with CRUD operations for cart management

---
+++ back-end/controllers/cartController.js
@@ -0,0 +1,44 @@
+const { db } = require("../models");
+const CartService = require("../services/CartService");
+const cartService = new CartService(db);
+const { success } = require("../utils/response");
+
+async function getCart(req, res) {
+  const userId = req.user.id;
+  const cart = await cartService.getCartWithItems(userId);
+  return success(res, 200, "Cart retrieved", { cart });
+}
+
+async function addItem(req, res) {
+  const userId = req.user.id;
+  const { productId, quantity } = req.body;
+  const cart = await cartService.addItem(userId, productId, quantity);
+  return success(res, 201, "Item added to cart", { cart });
+}
+
+async function updateCartItem(req, res) {
+  const userId = req.user.id;
+  const { productId } = req.params;
+  const { quantity } = req.body;
+  const cart = await cartService.updateItemQuantity(userId, productId, quantity);
+  return success(res, 200, "Cart updated", { cart });
+}
+
+async function removeCartItem(req, res) {
+  const userId = req.user.id;
+  const { productId } = req.params;
+  const cart = await cartService.removeItem(userId, productId);
+  return success(res, 200, "Item removed from cart", { cart });
+}
+
+async function checkoutCart(req, res) {
+  //checkoutservice.chekout
+}
+
+module.exports = {
+  getCart,
+  addItem,
+  updateCartItem,
+  removeCartItem,
+  checkoutCart,
+};
