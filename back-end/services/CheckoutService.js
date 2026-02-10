9e9358ebc6cbc03ec442c4cfeed484ae5705fb1e
fmSand <fridamsand@gmail.com>
2026-02-10 14:15:05 +0100

    feat(WIP!): add to checkout process by building order items, checking stock,  and assembling order with user, order and product data.

--- back-end/services/CheckoutService.js
+++ back-end/services/CheckoutService.js
@@ -16,15 +16,26 @@ class CheckoutService {
     try {
       //validate and get cart - cartservice
       const cartItems = await this.cartService.getCartItemsForCheckout(userId, transaction);
-      //build Order - orderservice
+
+      const orderItems = [];
       //check stock quantity - productservice
+      // for (const item of cartItems) { run checkstock? --> then push to orderItems if still available.
+      const stock = await this.productService.checkStock(productId, quantity, transaction); //this items productid and quantety
+
+      // cartitems --> push in (productids, quantites, unitPriceAtPurchase, productNameAtPurchase)--> orderitems
+
       //check membership - user/membershipservice
       const user = await this.userService.getById(userId, transaction);
       const membership = await this.membershipService.getById(user.membershipId, transaction);
-      const status = await this.orderStatusService.getByName("In Progress", transaction);
       //change orderstatus - orderstatusservice
-      // create order and order items, with discoint and membershipsnapshot.
+      const status = await this.orderStatusService.getByName("In Progress", transaction);
+
+      // create order:
+      // order {}--> data: userid,membershipNameAtPurchase, discountPercentAtPurchase, orderStatusId (totalamount etc if adding later) + items/products
+      const order = await this.orderService.create(orderData, orderItems, transaction);
+
       //handle stock /decrement (before or after clearing cart?)- productservice
+
       //calculate and update user membership if needed - membershipservice/userservice.
       const quantityToAdd = cartItems.reduce((sum, item) => sum + item.quantity, 0);
       const updatedUser = await this.userService.updateTotalPurchasedQuantity(userId, quantityToAdd, transaction);
@@ -33,8 +44,10 @@ class CheckoutService {
         transaction,
       );
       await this.userService.updateUserMembership(userId, newMembership, transaction);
+
       // Clear cart - cartservice
       await this.cartService.clearCart(cartId, transaction);
+
       //get and return order details - orderservice
 
       await transaction.commit();
