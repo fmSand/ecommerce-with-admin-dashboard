5a7c9d839b0140c35eb486db7f13138901736d3f
fmSand <fridamsand@gmail.com>
2026-02-10 12:48:50 +0100

    feat: update checkout method to use services methods

--- back-end/services/CheckoutService.js
+++ back-end/services/CheckoutService.js
@@ -10,21 +10,30 @@ class CheckoutService {
     this.orderStatusService = orderStatusService;
   }
 
-  async checkout(user) {
+  async checkout(userId) {
     //transaction
     //lock rows
     const transaction = await this.sequelize.transaction();
 
     try {
       //validate and get cart - cartservice
+      const cart = await this.cartService.getCartWithItems(userId);
       //build Order - orderservice
       //check stock quantity - productservice
       //check membership - user/membershipservice
+      const user = await this.userService.getById(userId, transaction);
+      const membership = await this.membershipService.getById(user.membershipId, transaction);
       //change orderstatus - orderstatusservice
       // create order and order items, with discoint and membershipsnapshot.
       //handle stock /decrement - productservice
       //calculate and update user membership if needed - membershipservice/userservice.
+      const newMembership = await this.membershipService.determineMembershipTier(
+        user.totalPurchasedQuantity,
+        transaction,
+      );
+      await this.userService.updateUserMembership(userId, newMembership, transaction);
       // Clear cart - cartservice
+      await this.cartService.clearCart(cartId, transaction);
       //get and return order details - orderservice
 
       await transaction.commit();
