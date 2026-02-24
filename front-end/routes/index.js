d1eaa6aac4c48bcd164eafbc37df80cf10a4184e
fmSand <fridamsand@gmail.com>
2026-02-24 22:49:04 +0100

    refactor: update home route to redirect based on user role and session token

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
+const { ADMIN_ROLE_ID } = require("../utils/constants");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  if (req.session?.token && req.session.user?.roleId === ADMIN_ROLE_ID) {
+    return res.redirect("/dashboard");
+  }
+  return res.redirect("/auth/login");
 });
 
 module.exports = router;
