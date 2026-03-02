5aa43bfe0fe4117b636a8565b646f53a5ccf6ba4
fmSand <fridamsand@gmail.com>
2026-03-02 19:29:20 +0100

    fix🙈: update API_BASE_URL to use the correct localhost port

--- front-end/utils/apiClient.js
+++ front-end/utils/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_URL || "http://localhost:3001";
+const API_BASE_URL = process.env.API_URL || "http://localhost:3000";
 
 async function request(method, path, req, body) {
   const headers = {};
